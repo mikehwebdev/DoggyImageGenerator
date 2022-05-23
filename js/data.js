07497b382c77f7385483959819deaddb8ddffa1b
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2022-05-23 11:46:33 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,580 @@
+export const dogArray = [{
+    apiSearchName:"affenpinscher",
+    actualName:"Affenpinscher"
+},
+{
+    apiSearchName: "african",
+    actualName: "African"
+},
+{
+    apiSearchName: "airedale",
+    actualName:"Airedale"
+},
+{
+    apiSearchName: "akita",
+    actualName: "Akita"
+},
+{   apiSearchName: "appenzeller",
+    actualName:"Appenzeller"
+},
+{
+    apiSearchName: "australian/shepherd",
+    actualName:"Australian Shepherd"
+},
+{   apiSearchName: "basenji",
+    actualName: "Basenji"
+},
+{
+    apiSearchName: "beagle",
+    actualName:"Beagle"
+},
+{
+    apiSearchName: "bluetick",
+    actualName:"Bluetick"
+},
+{
+    apiSearchName: "borzoi",
+    actualName:"Borzoi"
+},
+{
+    apiSearchName: "bouvier",
+    actualName:"Bouvier"
+},
+{
+    apiSearchName: "boxer",
+    actualName:"Boxer"
+},
+{
+    apiSearchName: "brabancon",
+    actualName:"Brabancon"
+},
+{
+    apiSearchName: "briard",
+    actualName:"Briard"
+},
+{
+    apiSearchName: "buhund/norwegian",
+    actualName:"Norwegian Buhund"
+},
+{
+    apiSearchName: "bulldog/boston",
+    actualName:"Boston Bulldog"
+},
+{
+    apiSearchName: "bulldog/english",
+    actualName:"English Bulldog"
+},
+{
+    apiSearchName: "bulldog/french",
+    actualName:"French Bulldog"
+},
+{
+    apiSearchName: "bullterrier/staffordshire",
+    actualName:"Staffordshire Bull Terrier"
+},
+{
+    apiSearchName: "cattledog/australian",
+    actualName:"Australian Cattle Dog"
+},
+{
+    apiSearchName: "chihuahua",
+    actualName:"Chihuahua"
+},
+{
+    apiSearchName: "chow",
+    actualName:"Chow"
+},
+{
+    apiSearchName: "clumber",
+    actualName:"Clumber"
+},
+{
+    apiSearchName: "cockapoo",
+    actualName:"Cockapoo"
+},
+{
+    apiSearchName: "collie/border",
+    actualName:"Border Collie"
+},
+{
+    apiSearchName: "coonhound",
+    actualName:"Coonhound"
+},
+{
+    apiSearchName: "corgi/cardigan",
+    actualName:"Cardigan Corgi"
+},
+{
+    apiSearchName: "cotondetulear",
+    actualName:"Cndetulear"
+},
+{
+    apiSearchName: "dachshund",
+    actualName:"Dachshund"
+},
+{
+    apiSearchName: "dalmatian",
+    actualName:"Dalmatian"
+},
+{
+    apiSearchName: "dane/great",
+    actualName:"Great Dane"
+},
+{
+    apiSearchName: "deerhound/scottish",
+    actualName:"Scottish Deerhound"
+},
+{
+    apiSearchName: "dhole",
+    actualName:"Dhole"
+},
+{
+    apiSearchName: "dingo",
+    actualName:"Dingo"
+},
+{
+    apiSearchName: "doberman",
+    actualName:"Doberman"
+},
+{
+    apiSearchName: "elkhound/norwegian",
+    actualName:"Norwegian Elkhound"
+},
+{
+    apiSearchName: "entlebucher",
+    actualName:"Entlebucher"
+},
+{
+    apiSearchName: "eskimo",
+    actualName:"Eskimo"
+},
+{
+    apiSearchName: "finnish/lapphund",
+    actualName:"Finnish Lapphund"
+},
+{
+    apiSearchName: "frise/bichon",
+    actualName:"Bichon Frise"
+},
+{
+    apiSearchName: "germanshepherd",
+    actualName:"German Shepherd"
+},
+{
+    apiSearchName: "greyhound/italian",
+    actualName:"Italian Greyhound"
+},
+{
+    apiSearchName: "groenendael",
+    actualName:"Groenendael"
+},
+{
+    apiSearchName: "havanese",
+    actualName:"Havanese"
+},
+{
+    apiSearchName: "hound/afghan",
+    actualName:"Afghan Hound"
+},
+{
+    apiSearchName: "hound/basset",
+    actualName:"Basset Hound"
+},
+{
+    apiSearchName: "hound/blood",
+    actualName:"Bloodhound"
+},
+{
+    apiSearchName: "hound/english",
+    actualName:"English Hound"
+},
+{
+    apiSearchName: "hound/ibizan",
+    actualName:"Ibizan Hound"
+},
+{
+    apiSearchName: "hound/plott",
+    actualName:"Plott Hound"
+},
+{
+    apiSearchName: "hound/walker",
+    actualName:"Walker Hound"
+},
+{
+    apiSearchName: "husky",
+    actualName:"Husky"
+},
+{
+    apiSearchName: "keeshond",
+    actualName:"Keeshond"
+},
+{
+    apiSearchName: "kelpie",
+    actualName:"Kelpie"
+},
+{
+    apiSearchName: "komondor",
+    actualName:"Komondor"
+},
+{
+    apiSearchName: "kuvasz",
+    actualName:"Kuvasz"
+},
+{
+    apiSearchName: "labradoodle",
+    actualName:"Labradoodle"
+},
+{
+    apiSearchName: "labrador",
+    actualName:"Labrador"
+},
+{
+    apiSearchName: "leonberg",
+    actualName:"Leonberg"
+},
+{
+    apiSearchName: "lhasa",
+    actualName:"Lhasa"
+},
+{  
+    apiSearchName: "malamute",
+    actualName:"Malamute"
+},
+{
+    apiSearchName: "malinois",
+    actualName:"Malinois"
+},
+{
+    apiSearchName: "maltese",
+    actualName:"Maltese"
+},
+{
+    apiSearchName: "mastiff/bull",
+    actualName:"Bull Mastiff"
+},
+{
+    apiSearchName: "mastiff/english",
+    actualName:"English Mastiff"
+},
+{
+    apiSearchName: "mastiff/tibetan",
+    actualName:"Tibetan Mastiff"
+},
+{
+    apiSearchName: "mexicanhairless",
+    actualName:"Mexican Hairless"
+},
+{
+    apiSearchName: "mix",
+    actualName:"Mix"
+},
+{
+    apiSearchName: "mountain/bernese",
+    actualName:"Bernese Mountain Dog"
+},
+{
+    apiSearchName: "mountain/swiss",
+    actualName:"Swiss Mountain Dog"
+},
+{
+    apiSearchName: "newfoundland",
+    actualName:"Newfoundland"
+},
+{
+    apiSearchName: "otterhound",
+    actualName:"Otterhound"
+},
+{
+    apiSearchName: "ovcharka/caucasian",
+    actualName:"Caucasian Ovcharka"
+},
+{
+    apiSearchName: "papillon",
+    actualName:"Papillon"
+},
+{
+    apiSearchName: "pekinese",
+    actualName:"Pekinese"
+},
+{
+    apiSearchName: "pembroke",
+    actualName:"Pembroke"
+},
+{
+    apiSearchName: "pinscher/miniature",
+    actualName:"Miniature Pinscher"
+},
+{
+    apiSearchName: "pitbull",
+    actualName:"Pitbull"
+},
+{
+    apiSearchName: "pointer/german",
+    actualName:"German pointer"
+},
+{
+    apiSearchName: "pointer/germanlonghair",
+    actualName:"German Longhair Pointer"
+},
+{
+    apiSearchName: "pomeranian",
+    actualName:"Pomeanian"
+},
+{
+    apiSearchName: "poodle/miniature",
+    actualName:"Miniature Poodle"
+},
+{
+    apiSearchName: "poodle/standard",
+    actualName:"Standard Poodle"
+},
+{
+    apiSearchName: "poodle/toy",
+    actualName:"Toy Poodle"
+},
+{
+    apiSearchName: "pug",
+    actualName:"Pug"
+},
+{
+    apiSearchName: "puggle",
+    actualName:"Puggle"
+},
+{
+    apiSearchName: "pyrenees",
+    actualName:"Pyrenees"
+},
+{
+    apiSearchName: "redbone",
+    actualName:"Redbone"
+},
+{
+    apiSearchName: "retriever/chesapeake",
+    actualName:"Chesapeake Retreiver"
+},
+{
+    apiSearchName: "retriever/curly",
+    actualName:"Curly Retreiver"
+},
+{
+    apiSearchName: "retriever/flatcoated",
+    actualName:"Flatcoated Retreiver"
+},
+{
+    apiSearchName: "retriever/golden",
+    actualName:"Golden Retreiver"
+},
+{
+    apiSearchName: "ridgeback/rhodesian",
+    actualName:"Rhodesian Ridgeback"
+},
+{
+    apiSearchName: "rottweiler",
+    actualName:"Rottweiler"
+},
+{
+    apiSearchName: "saluki",
+    actualName:"Saluki"
+},
+{
+    apiSearchName: "samoyed",
+    actualName:"Samoyed"
+},
+{
+    apiSearchName: "schipperke",
+    actualName:"Schipperke"
+},
+{
+    apiSearchName: "schnauzer/giant",
+    actualName:"Giant Schnauzer"
+},
+{
+    apiSearchName: "schnauzer/miniature",
+    actualName:"Miniature Schnauzer"
+},
+{
+    apiSearchName: "setter/english",
+    actualName:"English Setter"
+},
+{
+    apiSearchName: "setter/gordon",
+    actualName:"Gordon Setter"
+},
+{
+    apiSearchName:  "setter/irish",
+    actualName:"Irish Setter"
+},
+{
+    apiSearchName: "sheepdog/english",
+    actualName:"English Sheepdog"
+},
+{
+    apiSearchName: "sheepdog/shetland",
+    actualName:"Shetland Sheepdog"
+},
+{
+    apiSearchName: "shiba",
+    actualName:"Shiba"
+},
+{
+    apiSearchName: "shihtzu",
+    actualName:"Shihtzu"
+},
+{
+    apiSearchName: "spaniel/blenheim",
+    actualName:"Blenheim Spaniel"
+},
+{
+    apiSearchName: "spaniel/brittany",
+    actualName:"Brittany Spaniel"
+},
+{
+    apiSearchName: "spaniel/cocker",
+    actualName:"Cocker Spaniel"
+},
+{
+    apiSearchName: "spaniel/irish",
+    actualName:"Irish Spaniel"
+},
+{
+    apiSearchName: "spaniel/japanese",
+    actualName:"Japanese Spaniel"
+},
+{
+    apiSearchName: "spaniel/sussex",
+    actualName:"Sussex Spaniel"
+},
+{
+    apiSearchName: "spaniel/welsh",
+    actualName:"Welsh Spaniel"
+},
+{
+    apiSearchName: "spaniel/springer",
+    actualName:"Springer Spaniel"
+},
+{
+    apiSearchName: "spaniel/english",
+    actualName:"English Spaniel"
+},
+{
+    apiSearchName: "stbernard",
+    actualName:"St Bernard"
+},
+{
+    apiSearchName: "terrier/ american",
+    actualName:"American terrier"
+},
+{
+    apiSearchName: "terrier/australian",
+    actualName:"Australian terrier"
+},
+{
+    apiSearchName: "terrier/bedlington",
+    actualName:"Bedlington Terrier"
+},
+{
+    apiSearchName: "terrier/border",
+    actualName:"Border Terrier"
+},
+{
+    apiSearchName: "terrier/cairn",
+    actualName:"Cairn Terrier"
+},
+{
+    apiSearchName: "terrier/dandie",
+    actualName:"Dandie Terrier"
+},
+{
+    apiSearchName: "terrier/fox",
+    actualName:"Fox Terrier"
+},
+{
+    apiSearchName: "terrier/irish",
+    actualName:"Irish Terrier"
+},
+{
+    apiSearchName: "terrier/kerryblue",
+    actualName:"Kerryblue Terrier"
+},
+{
+    apiSearchName: "terrier/lakeland",
+    actualName:"Lakeland Terrier"
+},
+{
+    apiSearchName: "terrier/norfolk",
+    actualName:"Norfolk Terrier"
+},
+{
+    apiSearchName: "terrier/norwich",
+    actualName:"Norwich Terrier"
+},
+{
+    apiSearchName: "terrier/patterdale",
+    actualName:"Patterdale Terrier"
+},
+{
+    apiSearchName: "terrier/russell",
+    actualName:"Russell Terrier"
+},
+{
+    apiSearchName: "terrier/scottish",
+    actualName:"Scottish Terrier"
+},
+{
+    apiSearchName: "terrier/sealyham",
+    actualName:"Sealyham Terrier"
+},
+{
+    apiSearchName: "terrier/silky",
+    actualName:"Silky Terrier"
+},
+{
+    apiSearchName: "terrier/tibetan",
+    actualName:"Tibetan Terrier"
+},
+{
+    apiSearchName: "terrier/toy",
+    actualName:"Toy Terrier"
+},
+{
+    apiSearchName: "terrier/welsh",
+    actualName:"Welsh terrier"
+},
+
+{
+    apiSearchName: "terrier/westhighland",
+    actualName:"Westhighland terrier"
+},
+{
+    apiSearchName: "terrier/wheaten",
+    actualName:"Wheaten Terrier"
+},
+{
+    apiSearchName: "terrier/yorkshire",
+    actualName:"Yorkshire Terrier"
+},
+{
+    apiSearchName: "tervuren",
+    actualName:"Tervuren"
+},
+{
+    apiSearchName: "vizsla",
+    actualName:"Vizsla"
+},
+{
+    apiSearchName: "waterdog/spanish",
+    actualName:"Spanish Waterdog"
+},
+{
+    apiSearchName: "weimaraner",
+    actualName:"Weimaraner"
+},
+{
+    apiSearchName: "whippet",
+    actualName:"Whippet"
+},
+{
+    apiSearchName: "wolfhound/irish",
+    actualName:"Irish Wolfhound"
+},
+]
---
+++ js/javascript.js
@@ -0,0 +1,171 @@
+import { dogArray } from "./data"
+
+
+
+    // search page logic
+const randomBreedEl = document.getElementById('randomBreedBtn')
+const randomDogEl = document.getElementById('randomDogBtn')
+
+const picklistEl = document.getElementById('picklist')
+const dogBtn = document.getElementById('dogBtn')
+
+const searchEl = document.getElementById('breedSearch')
+const searchResultsEl = document.getElementById('searchResults')
+const namedWelcomeEl = document.getElementById('named-welcome')
+
+
+// navigation/journey
+const letsGo = document.getElementById('lets-go')
+const submitName = document.getElementById('submit-name')
+const submitColour = document.getElementById('submit-colour')
+
+const welcomeTab = document.querySelector('.welcome')
+const nameTab = document.querySelector('.name')
+const colourPicker = document.querySelector('.colour-picker')
+const searchTab = document.querySelector('.search')
+
+const imagesEl = document.querySelector('.images')
+const nameValue = document.getElementById('name')
+const colourPageEl = document.getElementById('colour-page')
+const containerEl = document.querySelector('.container')
+const colourPickerInput = document.getElementById('colour-picker-input')
+const colourPickerInput2 = document.getElementById('colour-picker-input2')
+
+const swirlEl = document.getElementById('swirl')
+
+letsGo.addEventListener('click',() => {
+    nameValue.value = ''
+    welcomeTab.classList.toggle('active')
+    nameTab.classList.toggle('active')
+} )
+
+submitName.addEventListener('click',() => {
+    
+    localStorage.setItem("Name", nameValue.value)
+    let userName = localStorage.getItem("Name")
+    namedWelcomeEl.innerHTML = `<p>Hi ${userName} welcome to the Doggy Image Generator! </p>
+    
+    <p>It looks a bit boring at the moment doesn't it? Let's make it a bit more colourful! </p>
+    
+    <p>Please pick your two favourite colours.</p>`
+    
+    nameTab.classList.toggle('active')
+    colourPicker.classList.toggle('active')
+})
+
+
+
+submitColour.addEventListener('click',() => {
+    colourPicker.classList.toggle('active')
+    searchTab.classList.toggle('active')
+} )
+
+function imgsTransition() {
+    searchTab.classList.toggle('active')
+    imagesEl.classList.toggle('active')
+}
+
+colourPageEl.addEventListener('click', ()=>{
+    console.log(colourPickerInput.value)
+    containerEl.style.background = `linear-gradient(${colourPickerInput.value}, ${colourPickerInput2.value})`;
+})
+
+swirlEl.addEventListener('click', () => {
+    let randomDegrees = Math.floor(Math.random() * 360 +1)
+    containerEl.style.background = `linear-gradient(${randomDegrees}deg, ${colourPickerInput.value}, ${colourPickerInput2.value})`;
+})
+
+// API logic
+
+dogBtn.addEventListener('click', fetchADog)
+
+
+function fetchADog () {
+let breedData = picklistEl.value
+fetch (`https://dog.ceo/api/breed/${breedData}/images/random/3`)
+.then (res => res.json())
+.then (data => {
+    
+imagesEl.innerHTML =`
+<img class="dog-pics" src="${data.message[0]}">
+<img class="dog-pics"  src="${data.message[1]}">
+<img class="dog-pics"  src="${data.message[2]}">`}
+)
+
+imgsTransition()
+}
+
+randomBreedEl.addEventListener('click', randomBreed)
+
+function randomBreed() {
+    let randomIndex = dogArray[Math.floor(Math.random () * dogArray.length)].apiSearchName
+    let breedData = randomIndex
+fetch (`https://dog.ceo/api/breed/${breedData}/images/random/3`)
+.then (res => res.json())
+.then (data => {
+imagesEl.innerHTML =`
+<img class="dog-pics" src="${data.message[0]}">
+<img class="dog-pics"  src="${data.message[1]}">
+<img class="dog-pics"  src="${data.message[2]}">
+`}
+)
+
+imgsTransition()
+}
+
+randomDogEl.addEventListener('click', randomDog)
+
+function randomDog() {
+    fetch (`https://dog.ceo/api/breeds/image/random/3`)
+.then (res => res.json())
+.then (data => {
+    
+imagesEl.innerHTML =`
+<img class="dog-pics" src="${data.message[0]}">
+<img class="dog-pics"  src="${data.message[1]}">
+<img class="dog-pics"  src="${data.message[2]}">
+`}
+)
+
+imgsTransition()
+}
+
+searchEl.addEventListener('keyup', breedSearch)
+
+function breedSearch(){
+    searchResultsEl.innerHTML = ''
+    const searchText = searchEl.value[0].toUpperCase() + searchEl.value.slice(1)
+    const searchResult = dogArray.filter(dog => dog.actualName.includes(searchText)).slice(0,5).sort()
+    
+    
+    for (let i = 0; i<searchResult.length; i++)
+    {
+        searchResultsEl.innerHTML +=`
+        <a onClick="searchResultFetch('${searchResult[i].apiSearchName}')" href="#">${searchResult[i].actualName}</a>
+        ` 
+    }
+}
+
+function searchResultFetch(breedData) {
+    
+    
+fetch (`https://dog.ceo/api/breed/${breedData}/images/random/3`)
+.then (res => res.json())
+.then (data => {
+    data.message[0] ? imagesEl.innerHTML +=
+    `<img class="dog-pics" src="${data.message[0]}">` : `<img class="dog-pics" src="https://images.unsplash.com/photo-1423958950820-4f2f1f44e075?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170">` 
+    data.message[1] ? imagesEl.innerHTML +=
+    `<img class="dog-pics" src="${data.message[1]}">` : imagesEl.innerHTML += `<img class="dog-pics" src="https://images.unsplash.com/photo-1423958950820-4f2f1f44e075?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170">` 
+    data.message[2] ? imagesEl.innerHTML +=
+    `<img class="dog-pics" src="${data.message[2]}">` : imagesEl.innerHTML +=`<img class="dog-pics" src="https://images.unsplash.com/photo-1423958950820-4f2f1f44e075?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170">` 
+
+}
+)
+
+imgsTransition()
+}
+    
+
+
+   
+
